7af8102fb9f1ec99345b4ab687d191f99da9897e
Ichimo <cmj14016@ict.nitech.ac.jp>
2022-10-05 19:14:06 +0900

    loads

---
+++ scriptfile.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    const spinner = document.getElementById('loading');
+    spinner.classList.add('loaded');
+  }
+
+//safari対策
+/*
+window.onpageshow = function (event) {
+    if (event.persisted) {
+        window.location.reload();
+    }
+}
+
+*/;
